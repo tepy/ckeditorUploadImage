c1569ee377b9de56b68ac7811971900df2e40412
Tepy Septiawan <tepy_septiawan@ymail.com>
2019-12-03 15:09:10 +0700

    edit routes

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', "CkeditorController@index");
 
 Route::get("ckeditor", "CkeditorController@index");
 Route::post('ckeditor/upload', 'CkeditorController@upload')->name('ckeditor.upload');
