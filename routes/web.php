72a97d2c1d860d3929a0d9f115478981d03c3f7f
Tepy Septiawan <tepy_septiawan@ymail.com>
2019-12-03 15:14:51 +0700

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', "CkeditorController@index");
+
+Route::get("ckeditor", "CkeditorController@index");
+Route::post('ckeditor/upload', 'CkeditorController@upload')->name('ckeditor.upload');
